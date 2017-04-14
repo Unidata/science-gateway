421a0b01d55bce990bbc6f45557d63bcc13b6d97
Julien Chastang <julien.c.chastang@gmail.com>
2017-04-14 15:28:21 -0600

    parameterizing boot.sh with an image UID

--- openstack/boot.sh
+++ openstack/boot.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 usage="$(basename "$0") [-h] [-n, --name vm name] [-k, --key key name] \n
-    [-s, --size vm size] [-ip, --ip ip address] -- script to create VMs.:\n
+    [-s, --size vm size] [-i, --image image UID] [-ip, --ip ip address] \n
+    -- script to create VMs:\n
     -h  show this help text\n
     -n, --name vm name\n
     -k, --key key name\n
     -s, --size vm size\n
-    -net, --netname network name or network UUID\n
+    -i, --image image UID\n
     -ip, --ip vm ip number\n"
+
+echo $usage
+
+exit 1
 
 while [[ $# > 0 ]]
 do
@@ -25,8 +30,8 @@ do
             VM_SIZE="$2"
             shift # past argument
             ;;
-        -net|--netname)
-            NET_NAME="$2"
+        -i|--image)
+            IMAGE_NAME="$2"
             shift # past argument
             ;;
         -ip|--ip)
@@ -71,35 +76,21 @@ if [ -z "$IP" ];
       exit 1
 fi
 
-if [ -z "$NET_NAME" ];
+if [ -z "$IMAGE_NAME" ];
    then
-      NET_NAME=${OS_PROJECT_NAME}-api-net
-      echo "No network name supplied to going with ${NET_NAME}." 
+      IMAGE_NAME=afbf353a-f0b5-4598-89bd-38acdc6f4b10
+      echo "No image name supplied so going with ${IMAGE_NAME}."
 fi
 
 # OS_PROJECT_NAME is defined in openrc.sh
 MACHINE_NAME=${OS_PROJECT_NAME}-${VM_NAME}
 
-# The image name below is hard-coded. Select from one of these image names which you can
-# get with `glance image-list | grep -i featured`
-# 
-# |--------------------------------------+-----------------------------------------------------|
-# | UUID                                 | Name                                                |
-# |--------------------------------------+-----------------------------------------------------|
-# | 21c904b7-b7b0-4f30-bb99-09aa2412bc3c | JS-API-Featured-CentOS6-Feb-10-2017                 |
-# | 736e206d-9c2c-4369-88db-8c3293bd2ad7 | JS-API-Featured-Centos7-Feb-7-2017                  |
-# | 58aebfd2-6ce4-4bcf-8da2-2a9bea6beb2f | JS-API-Featured-CentOS7-Intel-Developer-Feb-20-2017 |
-# | b8c5b987-7221-48d8-adb2-27fdcfbb38ba | JS-API-Featured-Ubuntu14-Feb-23-2017                |
-# | afbf353a-f0b5-4598-89bd-38acdc6f4b10 | JS-API-Featured-Ubuntu16-Feb-9-2017                 |
-# |--------------------------------------+-----------------------------------------------------|
-# 
-
 nova boot ${MACHINE_NAME} \
   --flavor ${VM_SIZE} \
-  --image afbf353a-f0b5-4598-89bd-38acdc6f4b10 \
+  --image ${IMAGE_NAME} \
   --key-name ${KEY_NAME} \
   --security-groups global-ssh-22 \
-  --nic net-name=${NET_NAME}
+  --nic net-name=${OS_PROJECT_NAME}-api-net
 
 # give chance for VM to fire up
 echo sleep 30 for seconds while VM fires up
