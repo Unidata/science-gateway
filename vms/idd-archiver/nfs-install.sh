e46b23e818df44cfdcfb38341219f258d87f0f3b
Julien Chastang <julien.c.chastang@gmail.com>
2017-05-23 12:35:15 -0600

    scriptifying nfs

---
+++ vms/idd-archiver/nfs-install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+echo /data		10.0.0.18(rw,sync,no_subtree_check) | tee --append /etc/exports > /dev/null
+
+exportfs -a
+service nfs-kernel-server start
+
+update-rc.d nfs-kernel-server defaults
