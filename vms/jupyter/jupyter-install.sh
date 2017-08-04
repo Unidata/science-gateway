2ca5f3e1792afd9b26fe5a3157553c191e237767
Julien Chastang <julien.c.chastang@gmail.com>
2017-08-04 15:03:25 -0600

    http --> https redirect, misc.

--- vms/jupyter/jupyter-install.sh
+++ vms/jupyter/jupyter-install.sh
@@ -1,7 +1,13 @@
+#!/bin/bash
 mkdir -p ~/config/
 cp jupyterhub_config.py ~/config/
 
+mkdir -p ~/nginx/
+cp nginx.conf ~/nginx/
+
 mkdir -p ~/logs/jupyter/
+
+mkdir -p ~/logs/nginx/
 
 mkdir -p ~/config/ssl/
 
