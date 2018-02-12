4a345342bcc865cb019ee60e07d96e417887ddcc
Julien Chastang <julien.c.chastang@gmail.com>
2018-02-12 09:19:42 -0700

    prepping for a thredds-test VM

--- vms/thredds/nfs-install.sh
+++ vms/thredds/nfs-install.sh
@@ -6,6 +6,6 @@ fi
 
 # create the NFS mount point
 mkdir -p /data
-mount 10.0.0.4:/data /data
+mount 10.0.0.8:/data /data
 
-echo 10.0.0.4:/data    /data   nfs rsize=32768,wsize=32768,timeo=14,intr | tee --append /etc/fstab > /dev/null
+echo 10.0.0.8:/data    /data   nfs rsize=32768,wsize=32768,timeo=14,intr | tee --append /etc/fstab > /dev/null
