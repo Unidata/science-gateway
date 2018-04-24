1e0008b48a5eb8caa68fbe0ad524fcd5f6cd53f2
Julien Chastang <julien.c.chastang@gmail.com>
2018-04-24 10:07:15 -0600

    fixing permissions and ownership issues

--- vms/jupyter/runjupyter.sh
+++ vms/jupyter/runjupyter.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
+adduser --disabled-password --gecos "" jupyter
+
+# Reset directories to baseline jupyter user. After user logs in for the first
+# time, they will reassert ownership for their directories (see
+# jupyterhub_config.py). This action is to prevent confusing (although
+# inconsequential) ownership in the /notebooks directory due to UID mismatches
+# from one container instantiating to the next.
+for i in /notebooks/*
+do
+    chown jupyter:docker $i
+    # Restrict directories to owner only
+    chmod 711 $i
+done
+
 jupyterhub -f /etc/jupyterhub/jupyterhub_config.py >> /var/log/jupyterhub.log 2>&1
