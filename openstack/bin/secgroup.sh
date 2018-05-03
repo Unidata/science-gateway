0799f06fef50b82618a783494260b504093c81da
Julien Chastang <julien.c.chastang@gmail.com>
2018-05-03 15:05:44 -0600

    more control over remote IP

--- openstack/bin/secgroup.sh
+++ openstack/bin/secgroup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-usage="$(basename "$0") [-h] [-n, --name secgroup name] [-p, --port secgroup port] -- 
-script to setup secgroups.:\n
+usage="$(basename "$0") [-h] [-n, --name secgroup name] \n
+[-r, --remote-ip] [-p, --port secgroup port] --
+script to setup secgroups:\n
     -h  show this help text\n
     -l, --list secgroup list\n
     -n, --name secgroup name\n
+    -r, --remote-ip allowable remote IPs (CIDR notation)\n
     -p, --port secgroup port\n"
 
 while [[ $# > 0 ]]
@@ -13,6 +15,10 @@ do
     case $key in
         -n|--name)
             SECGROUP_NAME="$2"
+            shift # past argument
+            ;;
+        -r|--remote-ip)
+            REMOTE_IP="$2"
             shift # past argument
             ;;
         -p|--port)
@@ -38,6 +44,12 @@ if [ -z "$SECGROUP_NAME" ];
       exit 1
 fi
 
+if [ -z "$REMOTE_IP" ];
+  then
+      $REMOTE_IP="0.0.0.0/0"
+      echo "No remote IP so going with 0.0.0.0/0 (i.e., everyone)."
+fi
+
 if [ -z "$SECGROUP_PORT" ];
    then
       echo "Must supply a secgroup port:" 
@@ -45,7 +57,11 @@ if [ -z "$SECGROUP_PORT" ];
       exit 1
 fi
 
-openstack security group create --description "${SECGROUP_NAME} & icmp enabled" global-${SECGROUP_NAME}
-openstack security group rule create --protocol tcp --dst-port ${SECGROUP_PORT}:${SECGROUP_PORT} --remote-ip 0.0.0.0/0 global-${SECGROUP_NAME}
-openstack security group rule create --protocol icmp global-${SECGROUP_NAME}
+
+openstack security group create --description "${SECGROUP_NAME} & icmp enabled" \
+          ${SECGROUP_NAME}
+openstack security group rule create --protocol tcp \
+          --dst-port ${SECGROUP_PORT}:${SECGROUP_PORT} \
+          --remote-ip ${REMOTE_IP} ${SECGROUP_NAME}
+openstack security group rule create --protocol icmp ${SECGROUP_NAME}
 openstack security group list
