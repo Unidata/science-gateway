222832a31b2c01f9d9553dacbccc81b323fb3432
Julien Chastang <julien.c.chastang@gmail.com>
2018-06-06 17:05:50 -0600

    redoing backup

--- vms/jupyter/setup-backup.sh
+++ vms/jupyter/setup-backup.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-git clone https://github.com/laurent22/rsync-time-backup ~/rsync-time-backup \
-    && chmod +x ~/rsync-time-backup/rsync_tmbackup.sh
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+git clone https://github.com/laurent22/rsync-time-backup /tmp/rsync-time-backup && \
+    cp /tmp/rsync-time-backup/rsync_tmbackup.sh /usr/local/bin/
+
 mkdir -p -- "/wrangler/backup-notebooks"
+
 # Required by rsync-time-backup
 touch "/wrangler/backup-notebooks/backup.marker"
 
 (crontab -l ; echo \
-     "0 */1 * * * ~/xsede-jetstream/vms/jupyter/backup-notebooks.sh") | crontab -
+     "0 0 * * * /usr/local/bin/rsync_tmbackup.sh --rsync-set-flags \"--recursive --numeric-ids --no-perms --itemize-changes\"  /notebooks /wrangler/backup-notebooks") | crontab -
