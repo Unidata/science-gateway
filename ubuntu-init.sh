f763e8e096000be78e8ffd728ab6dc6b336dc14e
Julien Chastang <julien.c.chastang@gmail.com>
2020-06-29 11:22:29 -0600

    update docker-compose

--- ubuntu-init.sh
+++ ubuntu-init.sh
@@ -45,7 +45,7 @@ dpkg --configure -a && apt-get remove -y docker docker-engine docker.io \
 curl -sSL get.docker.com | sh
 usermod -aG docker ${DOCKER_USER}
 
-curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.26.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 
 chmod +x /usr/local/bin/docker-compose
 
