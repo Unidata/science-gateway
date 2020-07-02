a2c767c0089b1ed9b6d5ad6aa86806dd344d5b53
Julien Chastang <julien.c.chastang@gmail.com>
2020-07-01 23:03:49 -0600

    removing unnecessary stuff

--- centos-init.sh
+++ centos-init.sh
@@ -4,22 +4,6 @@ if [ "$EUID" -ne 0 ]; then
   exit
 fi
 
-usage="$(basename "$0") [-h] --
-script to setup Docker. Run as root. Think before your type:\n
-    -h  show this help text\n"
-
-while [[ $# > 0 ]]
-do
-    key="$1"
-    case $key in
-        -h|--help)
-            echo -e $usage
-            exit
-            ;;
-    esac
-    shift # past argument or value
-done
-
 DOCKER_USER=$(id -u -n)
 
 systemctl stop docker
