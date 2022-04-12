a8c43f12f13f316c7b5a051baaa678b858ac46f9
Julien Chastang <julien.c.chastang@gmail.com>
2022-04-12 17:19:50 -0600

    turns out, ssh path was never necessary

--- openstack/openstack.sh
+++ openstack/openstack.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-usage="$(basename "$0") [-h] [-s, --ssh ssh directory path] [-o, --openrc openrc.sh file path] \n
+usage="$(basename "$0") [-h] [-o, --openrc openrc.sh file path] \n
 -- script to  start OpenStack CL Docker container. \n Arguments must be supplied with fully qualified paths.\n
     -h  show this help text\n
-    -s, --ssh full path to ssh directory containing your OpenStack Jetstream key\n
     -o, --openrc full path to directory with openrc.sh file obtained from Jetstream (bin)\n"
 
 while [[ $# > 0 ]]
 do
     key="$1"
     case $key in
-        -s|--ssh)
-            SSH_DIR="$2"
-            shift # past argument
-            ;;
         -o|--openrc)
             OPENRC="$2"
             shift # past argument
@@ -26,13 +21,6 @@ do
     shift # past argument or value
 done
 
-if [ -z "$SSH_DIR" ];
-  then
-      echo "Must supply an .ssh directory:"
-      echo -e $usage
-      exit 1
-fi
-
 if [ -z "$OPENRC" ];
    then
       echo "Must supply an openrc.sh file:"
@@ -41,7 +29,6 @@ if [ -z "$OPENRC" ];
 fi
 
 docker run -it  \
-       -v ${SSH_DIR}:/home/openstack/.ssh/ \
        -v ${OPENRC}:/home/openstack/bin/openrc.sh \
        unidata/science-gateway /bin/bash
 
