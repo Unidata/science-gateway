76cc6dfd64d247c4a789f4cde3f3415785ed62e5
Julien Chastang <julien.c.chastang@gmail.com>
2022-04-21 14:26:33 -0600

    openstack.sh improvements

--- openstack/openstack.sh
+++ openstack/openstack.sh
@@ -29,6 +29,8 @@ if [ -z "$OPENRC" ];
 fi
 
 docker run -it  \
+       -v ${PWD}/bin:/home/openstack/bin/ \
+       -v ${PWD}/.bashrc:/home/openstack/.bashrc \
        -v ${OPENRC}:/home/openstack/bin/openrc.sh \
        unidata/science-gateway /bin/bash
 
