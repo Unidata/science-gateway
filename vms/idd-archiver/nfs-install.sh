a4dc55c08dc33d05551f5e88d54d11d95942d470
B. Espinoza <respinoza@ucar.edu>
2022-06-10 13:41:20 -0600

    Update NFS commands for RockyLinux8

--- vms/idd-archiver/nfs-install.sh
+++ vms/idd-archiver/nfs-install.sh
@@ -4,6 +4,10 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
+###
+# Add IPs of machines which will access NFS server
+###
+
 # adde
 echo /data		10.0.0.188(rw,sync,no_subtree_check) | tee \
     --append /etc/exports > /dev/null
@@ -14,7 +18,11 @@ echo /data		10.0.0.122(rw,sync,no_subtree_check) | tee \
 echo /data		10.0.0.143(rw,sync,no_subtree_check) | tee \
     --append /etc/exports > /dev/null
 
-exportfs -a
-service nfs-kernel-server start
+###
+# Start NFS server
+###
 
-update-rc.d nfs-kernel-server defaults
+exportfs -a
+# Ensure server starts on reboot
+systemctl enable nfs-server.service
+systemctl start nfs-server.service
