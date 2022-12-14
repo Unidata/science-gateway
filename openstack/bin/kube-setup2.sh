72d9e207a018334356833f423f0f460010fae7fb
Julien Chastang <julien.c.chastang@gmail.com>
2022-12-14 11:52:39 -0700

    kube-setup2.sh improvements

--- openstack/bin/kube-setup2.sh
+++ openstack/bin/kube-setup2.sh
@@ -16,17 +16,13 @@ mkdir -p $HOME/.kube/
 cp $HOME/jetstream_kubespray/inventory/$CLUSTER/artifacts/admin.conf \
     $HOME/.kube/config
 
-sed -i 's/10\.0\.[[:digit:]]\+\.[[:digit:]]\+/127.0.0.1/g' $HOME/.kube/config
-
-FLOATINGIPOFMASTER=$(openstack server list | grep $CLUSTER | \
-    grep -i master | awk 'BEGIN { FS = "|" } ; { print $5 }' | \
-    awk '{print $NF}')
+sed -i 's/10\.[[:digit:]]\+\.[[:digit:]]\+\.[[:digit:]]\+/127.0.0.1/g' $HOME/.kube/config
 
 echo When cluster is ready, ssh tunnel into master node:
-echo ssh ubuntu@${FLOATINGIPOFMASTER} -L 6443:localhost:6443
-echo Add your PKs to ~/.ssh/authorized_keys on ${FLOATINGIPOFMASTER}
+echo ssh ubuntu@${IP} -L 6443:localhost:6443
+echo Add your PKs to ~/.ssh/authorized_keys on ${IP}
 echo
 echo Add your email to \
      ~/jupyterhub-deploy-kubernetes-jetstream/setup_https/https_issuer.yml
 echo kubectl patch storageclass cinder-csi -p \
-     \'{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}\'
+     \'{\"metadata\": {\"annotations\":{\"storageclass.kubernetes.io/is-default-class\":\"true\"}}}\'
