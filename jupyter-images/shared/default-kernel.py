8aee284350171061904a0e3cdb525986b9feaff0
Ana Espinoza <respinoza@ucar.edu>
2023-08-14 16:27:34 -0600

    Improvements
    - build.sh now pulls in the newest base image
    - default-kernel.py received fixes
    - add .condarc to configure user def envs

--- jupyter-images/shared/default-kernel.py
+++ jupyter-images/shared/default-kernel.py
@@ -12,18 +12,23 @@ argv[2] -- directory to recursively search through (optional)
 from sys import argv
 from glob import glob
 from os import rename
+from os.path import join
 import json
 
 kernel = argv[1]
 directory = argv[2] if len(argv) > 2 else "./"
 
 for filename in glob('**/*.ipynb', root_dir=directory, recursive=True):
-    filename = directory+filename
+    filename = join(directory, filename)
     tmpfile = filename+'.tmp'
     print(filename)
     # Load notebook and edit metadata
     with open(filename, 'r') as f:
         data = json.load(f)
+        # First, ensure that the keys exist
+        data.setdefault('metadata', {}).setdefault('kernelspec', {}).setdefault('display_name', 'display_name')
+        data.setdefault('metadata', {}).setdefault('kernelspec', {}).setdefault('name', 'name')
+        # Set the values I want
         data['metadata']["kernelspec"]["display_name"] = "Python [conda env:{}]".format(kernel)
         data['metadata']["kernelspec"]["name"] = "conda-env-{}-py".format(kernel)
     # Do not dump to original file, bugs may occur if the new values are shorter
