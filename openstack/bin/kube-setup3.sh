6eb43c62151636b5ba59e8a3f095ca63e490f86b
Julien Chastang <julien.c.chastang@gmail.com>
2024-04-02 17:11:53 -0600

    letsencrypt@unidata.ucar.edu

--- openstack/bin/kube-setup3.sh
+++ openstack/bin/kube-setup3.sh
@@ -2,15 +2,10 @@
 
 set -e # Exit immediately if a command exits with a non-zero status.
 
-EMAIL="${1:-}"
-
 replace_email() {
-    local email="$1"
+    local email="letsencrypt@unidata.ucar.edu"
     local directory="${HOME}/jupyterhub-deploy-kubernetes-jetstream/setup_https"
-
-    if [ -n "$email" ]; then
-        grep -Rl 'YOUREMAIL' "$directory" | xargs sed -i "s/YOUREMAIL/$email/g"
-    fi
+    grep -Rl 'YOUREMAIL' "$directory" | xargs sed -i "s/YOUREMAIL/$email/g"
 }
 
 cd "${HOME}/jetstream_kubespray" || exit 1
@@ -19,4 +14,4 @@ cp "${HOME}/.ssh/unidata-tasks.yml" .
 
 ansible-playbook --become -i "inventory/${CLUSTER}/hosts" unidata-tasks.yml -b -v --limit "${CLUSTER}-1"
 
-replace_email "$EMAIL"
+replace_email
