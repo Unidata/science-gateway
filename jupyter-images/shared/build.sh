6d4d1f6ab852dedba7ac139f57f3de33f13bad41
Julien Chastang <julien.c.chastang@gmail.com>
2025-04-14 11:00:11 -0600

    add a Dockerfile parameter

--- jupyter-images/shared/build.sh
+++ jupyter-images/shared/build.sh
@@ -4,9 +4,10 @@
 set -e
 
 usage() {
-    echo "Usage: $0 <image-name> [--push]"
-    echo "  <image-name>  Name of the Docker image to build"
-    echo "  --push        Optionally push the Docker image to DockerHub"
+    echo "Usage: $0 <image-name> [--push] [-f <Dockerfile>]"
+    echo "  <image-name>    Name of the Docker image to build"
+    echo "  --push          Optionally push the Docker image to DockerHub"
+    echo "  -f <Dockerfile> Specify an alternate Dockerfile to use"
     exit 1
 }
 
@@ -17,10 +18,30 @@ fi
 
 IMAGE_NAME=$1
 PUSH_IMAGE=false
+DOCKERFILE="Dockerfile"
 
-if [ "$2" == "--push" ]; then
-    PUSH_IMAGE=true
-fi
+# Parse optional arguments
+shift
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --push)
+            PUSH_IMAGE=true
+            shift
+            ;;
+        -f)
+            if [ -z "$2" ]; then
+                echo "Error: -f requires a Dockerfile path"
+                usage
+            fi
+            DOCKERFILE="$2"
+            shift 2
+            ;;
+        *)
+            echo "Unknown option: $1"
+            usage
+            ;;
+    esac
+done
 
 DATE_TAG=$(date "+%Y%b%d_%H%M%S")
 RANDOM_HEX=$(openssl rand -hex 2)
@@ -28,8 +49,8 @@ TAG="${DATE_TAG}_${RANDOM_HEX}"
 FULL_TAG="unidata/$IMAGE_NAME:$TAG"
 
 # Build the Docker image
-echo "Building Docker image with tag: $FULL_TAG"
-docker build --no-cache --pull --tag "$FULL_TAG" .
+echo "Building Docker image with tag: $FULL_TAG using Dockerfile: $DOCKERFILE"
+docker build --no-cache --pull --tag "$FULL_TAG" -f "$DOCKERFILE" .
 
 echo "Docker image built successfully: $FULL_TAG"
 
